dee98dfbda3c644eb38fdb5118ef81aed6fa71bb
JaViLuMa <matej.gamer01@gmail.com>
2021-10-10 19:25:19 +0200

    Leave development only

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -18,36 +18,4 @@ module.exports = {
       directory: __dirname + '/db/seeds',
     },
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
 };
