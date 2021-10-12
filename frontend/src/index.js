0546dc0f9f73bcb643bb323fad78d0b1e5595796
JaViLuMa <matej.gamer01@gmail.com>
2021-10-12 16:29:07 +0200

    Get rid of all CSS files, add global styles styled component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createGlobalStyle } from 'styled-components';
 import { App } from './components';
 
 import 'antd/dist/antd.css';
-import './styles/global.css';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
